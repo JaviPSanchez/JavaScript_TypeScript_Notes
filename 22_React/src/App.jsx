eb70255f88197959d660dd9ea3b6d866446c3b58
JaviPSanchez <javier.palomino-sanchez@outlook.com>
2023-05-24 23:28:45 +0200

    Day 240523

---
+++ 22_React/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import ArrayMethods from "./components/ArrayMethods2022-Reverse-Sort-Splice";
+import GeneratorFunctionAsStateMachine from "./components/GeneratorFunctionAsStateMachine";
+
+function App() {
+  return (
+    <>
+      {/* <GeneratorFunctionAsStateMachine /> */}
+      <ArrayMethods />
+    </>
+  );
+}
+
+export default App;
---
+++ 22_React/src/components/ArrayMethods2022-Reverse-Sort-Splice.jsx
@@ -0,0 +1,5 @@
+import { useState } from "react";
+
+export default function ArrayMethods() {
+  return <div>ArrayMethods2022-Reverse-Sort-Splice</div>;
+}
