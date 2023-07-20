bc6f0a4bb6e7a04a32be6f0d117a5c7e7309c432
JaviPSanchez <javier.palomino-sanchez@outlook.com>
2023-07-20 23:39:49 +0200

    Finish Step component

--- 22_React/src/components/Steps.jsx
+++ 22_React/src/components/Steps.jsx
@@ -4,7 +4,7 @@ const FirstStep = ({ step }) => {
   return <p className="message">{`Step ${step}: Learn React 🧪`}</p>;
 };
 const SecondStep = ({ step }) => {
-  return <p className="message">{`Step ${step}: Enjoy the right 🏋️‍♀️`}</p>;
+  return <p className="message">{`Step ${step}: Enjoy the right 🏋️‍♀️ `}</p>;
 };
 const ThirdStep = ({ step }) => {
   return <p className="message">{`Step ${step}: Make friends 🙌`}</p>;
@@ -12,6 +12,7 @@ const ThirdStep = ({ step }) => {
 
 const Steps = () => {
   const [step, setStep] = useState(1);
+  const [open, setOpen] = useState(false);
 
   function handlePrevious() {
     if (step > 1) setStep(step - 1);
@@ -21,49 +22,58 @@ const Steps = () => {
   }
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <span
-          className={
-            step >= 1
-              ? 'step-number--active number'
-              : 'step-number--normal number'
-          }
-        >
-          1
-        </span>
-        <span
-          className={
-            step >= 2
-              ? 'step-number--active number'
-              : 'step-number--normal number'
-          }
-        >
-          2
-        </span>
-        <span
-          className={
-            step === 3
-              ? 'step-number--active number'
-              : 'step-number--normal number'
-          }
-        >
-          3
-        </span>
-      </div>
-      {step === 1 ? (
-        <FirstStep step={step} />
-      ) : step === 2 ? (
-        <SecondStep step={step} />
-      ) : (
-        <ThirdStep step={step} />
+    <div className="wrapper">
+      {open && (
+        <div className="steps">
+          <div className="numbers">
+            <span
+              className={
+                step >= 1
+                  ? 'step-number--active number'
+                  : 'step-number--normal number'
+              }
+            >
+              1
+            </span>
+            <span
+              className={
+                step >= 2
+                  ? 'step-number--active number'
+                  : 'step-number--normal number'
+              }
+            >
+              2
+            </span>
+            <span
+              className={
+                step === 3
+                  ? 'step-number--active number'
+                  : 'step-number--normal number'
+              }
+            >
+              3
+            </span>
+          </div>
+          <div className="message-container">
+            {step === 1 ? (
+              <FirstStep step={step} />
+            ) : step === 2 ? (
+              <SecondStep step={step} />
+            ) : (
+              <ThirdStep step={step} />
+            )}
+          </div>
+
+          <div className="buttons-container">
+            <button onClick={handlePrevious}>Previous</button>
+
+            <button onClick={handleNext}>Next</button>
+          </div>
+        </div>
       )}
-
-      <div className="buttons-container">
-        <button onClick={handlePrevious}>Previous</button>
-
-        <button onClick={handleNext}>Next</button>
-      </div>
+      <button onClick={() => setOpen(!open)} className="close">
+        &times;
+      </button>
     </div>
   );
 };
